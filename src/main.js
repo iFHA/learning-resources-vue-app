3e41f7da9c3bf49d937e04620f922bc1b034d541
fernando <fernando.everywhere@gmail.com>
2025-10-01 21:11:26 -0300

    add first components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import { createApp } from 'vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import App from './App.vue';
 
-createApp({}).mount('#app');
+const app = createApp(App);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+
+app.mount('#app');
